2ef806b09107c3c5aacad50643b9d882f45b5aa8
mtalaj00 <mtalaj00@fesb.hr>
2022-06-02 17:01:26 +0200

    test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
+            Console.WriteLine("vj1");
         }
     }
 }
